0c13899076193a716d35fcd472b9c9c5c9a49e73
ragundez <ragundez@qualogy.co>
2016-05-05 15:53:15 +0200

    wrapper for python2

---
+++ Makefile
@@ -0,0 +1,16 @@
+# location of the Python header files
+PYTHON_VERSION = 2.7
+PYTHON_INCLUDE = /usr/include/python$(PYTHON_VERSION)
+
+# location of the Boost Python include files and library
+BOOST_INC = /usr/include/python3.4
+BOOST_LIB = /usr/lib
+
+# compile mesh classes
+TARGET = basler_camera
+
+$(TARGET).so: $(TARGET).o
+	g++ -shared -Wl,--export-dynamic $(TARGET).o -L$(BOOST_LIB) -std=c++11 -L/usr/local/lib -I/ -L/opt/pylon5/lib64 -I/opt/pylon5/include -lpylonbase -lpylonutility -lGenApi_gcc_v3_0_Basler_pylon_v5_0 -lGCBase_gcc_v3_0_Basler_pylon_v5_0 -lboost_python -L/usr/lib/python$(PYTHON_VERSION)/config -lpython$(PYTHON_VERSION) -o $(TARGET).so
+
+$(TARGET).o: $(TARGET).cpp
+	g++ -I$(PYTHON_INCLUDE) -I$(BOOST_INC) -fPIC -c -std=c++11 $(TARGET).cpp -L/opt/pylon5/lib64 -L/usr/local/lib -I/ -L/opt/pylon5/lib64 -I/opt/pylon5/include -lpylonbase -lpylonutility -lGenApi_gcc_v3_0_Basler_pylon_v5_0 -lGCBase_gcc_v3_0_Basler_pylon_v5_0 -lboost_python
---
+++ basler_camera.cpp
@@ -0,0 +1,56 @@
+#include <boost/python.hpp>
+#include <pylon/PylonIncludes.h>
+#include <sstream>
+#include <iostream>
+#include <string>
+
+#ifdef PYLON_WIN_BUILD
+#include <pylon/PylonGUI.h>
+#endif
+
+using namespace Pylon;
+using namespace std;
+
+string capture()
+{
+    CPylonImage image;
+    CImageFormatConverter fc;
+    CGrabResultPtr ptrGrabResult;
+
+    // Automagically call PylonInitialize and PylonTerminate to ensure
+    // the pylon runtime system is initialized during the lifetime of this object.
+    Pylon::PylonAutoInitTerm autoInitTerm;
+
+    try
+    {
+        CInstantCamera camera( CTlFactory::GetInstance().CreateFirstDevice());
+        cout << "\nUsing device " << camera.GetDeviceInfo().GetModelName() << endl;
+
+        camera.Open();
+        camera.StartGrabbing();
+
+        fc.OutputPixelFormat = PixelType_RGB8packed;
+        camera.RetrieveResult( 5000, ptrGrabResult, TimeoutHandling_ThrowException);
+
+        if (ptrGrabResult->GrabSucceeded()){
+            fc.Convert(image, ptrGrabResult);
+            unsigned char* img = (unsigned char*)image.GetBuffer();
+            string string_bytes;
+            string_bytes.append(reinterpret_cast<const char*> (img));
+            return string_bytes;
+        }
+        else {
+            cout << "\nCould not grab image" << endl;
+            return "FAIL";
+        }
+    }catch(...){
+        cout << "\nThere are no active devices" << endl;
+        return "FAIL";
+    }
+}
+
+BOOST_PYTHON_MODULE(basler_camera)
+{
+    using namespace boost::python;
+    def("capture", capture);
+}
